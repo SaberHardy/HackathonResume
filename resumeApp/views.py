b9d2bdae4780ccba8c4d7b13aee89c57ee4967c7
SaberHardy <samirsamado5@gmail.com>
2022-09-20 18:30:29 +0100

    project steps

--- resumeApp/views.py
+++ resumeApp/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+"""
+ # TODO: 
+Who am I.(description)
+Education:
+    - Bachelor and Master
+Online Courses
+    - Coursera
+Interests:
+Work Experience:
+
+Skills and Frameworks(centred)(This should be with bars and percents)
+Hobbies
+Languages
+Projects
+
+Contact me.
+
+Footer
+
+Powered by me
+
+Button to navigate to top
+
+
+"""
